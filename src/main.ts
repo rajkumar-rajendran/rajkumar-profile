70899cc30de4d59e24e8783c950b192fbd099454
Rajkumar Rajendran <rajzp1@gmail.com>
2026-03-06 20:32:44 -0600

    Revamp design with tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { provideZoneChangeDetection } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withFetch())
+  ]
+}).catch(err => console.error(err));
